998293ed8eaf8e04cf8c85bbdf40a8deee71d046
iñaki sorovigarat <inakisorovigarat@Mac.home>
2025-04-03 18:21:56 +0200

    fix(build) vercel rejects .env regex

--- api/service.ts
+++ api/service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = 'https://prueba-tecnica-api-tienda-moviles.onrender.com/';
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
